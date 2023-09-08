b0d2494e7e835de8672a6dad9f284ea061bd0535
unamedkr <unamedkr@gmail.com>
2023-09-08 10:39:43 +0900

    datasets 수정

--- ezlm/data/datasets.py
+++ ezlm/data/datasets.py
@@ -7,35 +7,27 @@ def load_tokenized_dataset(tokenizer, data_style, train_dataset, max_length=512)
             return tokenizer(examples["text"], padding=True,
                             return_overflowing_tokens=True,
                             truncation=True, max_length=max_length, stride=10, return_offsets_mapping=True)
-        
-        # train_dataset_mapped = train_dataset.map(
-        #     tokenize_function,
-        #     batched=True,
-        #     remove_columns=["id", "url", "title", "text"]
-        # )
 
     if data_style == "alpaca":
         def tokenize_function(examples):
             converted_examples = []
             for idx in range(len(examples['instruction'])):
-                if len(examples['instruction'][idx]) <5:
+                if len(examples['instruction'][idx] or  examples['output'][idx]) <10:
                     continue
                 
                 text = ''
-                text += f"<s> [INST] {examples['instruction'][idx]} {examples['input'][idx]} [/INST]\n"
-                text += f"{examples['output'][idx]} \n </s>"
+                if(len(examples['input'][idx]) ==0):  
+                    text += f"<s> [INST] {examples['instruction'][idx]} [/INST]\n"
+                else:
+                    text += f"<s> [INST] {examples['instruction'][idx]} {examples['input'][idx]} [/INST]\n"
 
+                text += f"{examples['output'][idx]} \n </s>"
+                
                 converted_examples.append(text)
             
             return tokenizer(converted_examples, padding=False,
                      return_overflowing_tokens=True,
                      truncation=True, max_length=max_length, stride=10)
-        
-        # train_dataset_mapped = train_dataset.map(
-        #     tokenize_function,
-        #     batched=True,
-        #     remove_columns=['instruction', 'input', 'output']
-        # )
 
     if data_style == "wizard":
         train_dataset = train_dataset.remove_columns(['id'])
@@ -56,13 +48,6 @@ def load_tokenized_dataset(tokenizer, data_style, train_dataset, max_length=512)
                      return_overflowing_tokens=True,
                      truncation=True, max_length=max_length, stride=10)
 
-    
-        # train_dataset_mapped = train_dataset.map(
-        #     tokenize_function,
-        #     batched=True,
-        #     remove_columns=['conversations']
-        # )
-    
     columns = list(train_dataset.features.keys())
     train_dataset_mapped = train_dataset.map(
         tokenize_function,
