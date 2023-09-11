4102d1147b04d78de0472ceb185d2271d1ff00b8
unamedkr <unamedkr@gmail.com>
2023-09-11 18:27:45 +0900

    plankton 모델 추가

--- ezlm/tokenizers/tiger.py
+++ ezlm/tokenizers/tiger.py
@@ -1,18 +1,18 @@
-from transformers import AutoTokenizer
+# from transformers import AutoTokenizer
 
-import os
-from dotenv import load_dotenv 
+# import os
+# from dotenv import load_dotenv 
 
 
-load_dotenv('.env.dev')
-HF_TOKEN = os.environ.get("HF_TOKEN")
+# load_dotenv('.env.dev')
+# HF_TOKEN = os.environ.get("HF_TOKEN")
 
-class TigerTokenizer():
+# class TigerTokenizer():
     
-    def from_pretrained(model_path: str = 'quantumaikr/tiger', cache_dir="hub"):
-        tokenizer = AutoTokenizer.from_pretrained(model_path, token=HF_TOKEN, cache_dir=cache_dir)
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = 'right'
+#     def from_pretrained(model_path: str = 'quantumaikr/tiger', cache_dir="hub"):
+#         tokenizer = AutoTokenizer.from_pretrained(model_path, token=HF_TOKEN, cache_dir=cache_dir)
+#         tokenizer.pad_token = tokenizer.eos_token
+#         tokenizer.padding_side = 'right'
         
-        return tokenizer
+#         return tokenizer
     
