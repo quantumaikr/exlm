1aa57f9d41094f4d36766bca5bb0a546bd7d0c49
unamedkr <unamedkr@gmail.com>
2023-09-15 20:10:46 +0900

    chat.py 추가

---
+++ train-plankton-3B-48GB-ds.py
@@ -0,0 +1,67 @@
+from ezlm import PlanktonForCausalLM
+from ezlm import load_tokenized_dataset, print_model_size
+from ezlm.variables import *
+
+from datasets import load_dataset, concatenate_datasets
+
+from transformers import DataCollatorForLanguageModeling
+from transformers import Trainer, TrainingArguments
+
+from transformers import AutoTokenizer
+
+
+from ezlm import PlanktonConfig, PlanktonForCausalLM
+from ezlm import print_model_size
+
+
+
+base_model = 'KoreanLM-3B'
+tokenizer = AutoTokenizer.from_pretrained('quantumaikr/plankton_tokenizer')
+model = PlanktonForCausalLM.from_pretrained(base_model, cache_dir="hub")
+
+
+
+train_dataset = load_dataset("lcw99/wikipedia-korean-20221001", split="train[:]", cache_dir="hub")
+wiki_train_dataset = load_tokenized_dataset(tokenizer, 'kwiki', train_dataset)
+
+# train_dataset = load_dataset('junelee/sharegpt_deepl_ko', data_files='ko_alpaca_style_dataset.json', split="train[:]", cache_dir="hub")
+# sharegpt_train_dataset = load_tokenized_dataset(tokenizer, 'alpaca', train_dataset)
+
+# train_dataset = load_dataset('nlpai-lab/kullm-v2', split="train[:]", cache_dir="hub")
+# kullm_train_dataset = load_tokenized_dataset(tokenizer, 'alpaca', train_dataset, max_length=1024)
+
+# train_dataset = load_dataset('FreedomIntelligence/alpaca-gpt4-korean', split="train[:]", cache_dir="hub")
+# alpaca_gpt4_train_dataset = load_tokenized_dataset(tokenizer, 'wizard', train_dataset)
+
+
+# all_dataset = concatenate_datasets([wiki_train_dataset, sharegpt_train_dataset, kullm_train_dataset, alpaca_gpt4_train_dataset])
+all_dataset = concatenate_datasets([wiki_train_dataset])
+
+
+
+# 훈련 시작
+train_args = TrainingArguments(
+    output_dir="result",
+    num_train_epochs=1,
+    # per_device_train_batch_size=14,
+    per_device_train_batch_size=6,
+    gradient_accumulation_steps=2,
+    save_steps=500,
+    logging_steps=10,
+    save_strategy='steps',
+    save_safetensors=True,
+    save_total_limit=1,
+    # deepspeed=ZERO_2_SETTINGS,
+    deepspeed=ZERO_3_SETTINGS
+)
+
+trainer = Trainer(
+    model=model,
+    args=train_args,
+    data_collator=DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False),
+    train_dataset=all_dataset,
+)
+
+trainer.train()
+trainer.model.save_pretrained('KoreanLM-3B')
+# trainer.model.push_to_hub('plankton-1B-1024')
